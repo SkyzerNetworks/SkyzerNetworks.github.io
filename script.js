82c77098b0aad5c43a5306c442de494bfaeca7a7
Skyzer?!? <149444785+SkyzerNetworks@users.noreply.github.com>
2024-01-22 21:32:33 +0000

    Startup

---
+++ script.js
@@ -0,0 +1,15 @@
+// Javascript Code File
+
+// File Information
+
+// (START)
+
+/* (START)
+
+Date Created - 1 / 18 / 2024
+Version - 0.00.00 (Startup)
+Author - Michael
+Company - SkyzerNetworks
+Source Code - https://github.com/SkyzerNetworks/SkyzerNetworks.github.io
+
+(END) */
