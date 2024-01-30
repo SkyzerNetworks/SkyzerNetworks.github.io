09c25852b62d3e2bdd83e0fe7be45489bc29097f
Skyzer?!? <149444785+SkyzerNetworks@users.noreply.github.com>
2024-01-30 09:50:28 -0500

    Startup

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ var redirectC = 1; // 1 = on , 0 = off
 
 function redirect_contruction() {
     if (redirectC == 1) {
-        window.location.replace("https://SkyzerNetworks.github.io/construction-page/construction.html");
+        window.location.replace("/construction-page/construction.html");
     }
     else {
         console.log("No Redirect");
